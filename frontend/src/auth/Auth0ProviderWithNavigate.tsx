79753574e36225a3f56de022581fb14d84ed85ca
Patel Prince <princeptl2305@gmail.com>
2024-02-20 10:52:24 +0530

    Frontend: Auth0 Access Token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-import { useCreateMyUser } from "@/api/MyUserApi";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
-
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -17,11 +16,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Unable to intilise auth");
   }
 
-  const onRedirectCallbask = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ authoId: user.sub, email: user.email });
-    }
-    // console.log("User", user);
+  const onRedirectCallbask = () => {
+    navigate("/auth-callback");
   };
 
   return (
